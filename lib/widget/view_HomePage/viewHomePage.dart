4a595ed97dbbeba14872914f10e20981c7c315ed
AsrofunNiam <asrofunniam44@gmail.com>
2022-12-18 01:57:34 +0700

    widget Card

---
+++ lib/widget/view_HomePage/viewHomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ViewHomePageOne extends StatefulWidget {
+  const ViewHomePageOne({Key? key}) : super(key: key);
+
+  @override
+  State<ViewHomePageOne> createState() => _ViewHomePageOneState();
+}
+
+class _ViewHomePageOneState extends State<ViewHomePageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
