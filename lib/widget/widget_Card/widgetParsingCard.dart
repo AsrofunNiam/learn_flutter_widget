99b5d6c2654deb16ec5ae1d971d99d3d4ffd19db
AsrofunNiam <asrofunniam44@gmail.com>
2022-12-20 06:32:37 +0700

    list menus 2

--- lib/widget/widget_Card/widgetParsingCard.dart
+++ lib/widget/widget_Card/widgetParsingCard.dart
@@ -2,24 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:learn_flutter_widget/model/model_Card/modalParsingCard.dart';
 
 class WidgetParsingCard extends StatelessWidget {
-   const WidgetParsingCard({Key? key}) : super(key: key);
+  const WidgetParsingCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // var home;
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(
         title: const Text(" Parsing data widget Card"),
       ),
-      body:  Container(
+      body: Container(
         child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ModalParsingCard(iconCard: Icons.home, textCard: 'Test 1', colorCard: Color.fromARGB(255, 91, 130, 238)),
-            SizedBox(height: 30,),
-            ModalParsingCard(iconCard: Icons.book, textCard: 'Test 2', colorCard: Color.fromARGB(255, 88, 231, 207)),
-            SizedBox(height: 30,),
-            ModalParsingCard(iconCard: Icons.person, textCard: 'Test 3', colorCard: Color.fromARGB(255, 240, 124, 116)),
+            ModalParsingCard(
+                iconCard: Icons.home,
+                textCard: 'Test 1',
+                colorCard: Color.fromARGB(255, 91, 130, 238)),
+            SizedBox(
+              height: 30,
+            ),
+            ModalParsingCard(
+                iconCard: Icons.book,
+                textCard: 'Test 2',
+                colorCard: Color.fromARGB(255, 88, 231, 207)),
+            SizedBox(
+              height: 30,
+            ),
+            ModalParsingCard(
+                iconCard: Icons.person,
+                textCard: 'Test 3',
+                colorCard: Color.fromARGB(255, 240, 124, 116)),
           ],
         ),
       ),
